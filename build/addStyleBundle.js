72957188ef5b30cc95fc60c0e057c7dd497720f7
JayChase <JonathanChase@outlook.com>
2017-08-29 17:32:22 +0900

    changed style bundle

--- build/addStyleBundle.js
+++ build/addStyleBundle.js
@@ -3,7 +3,7 @@ var glob = require("glob");
 const index = fs.readFileSync('./src/index.html', 'utf8');
 const path = require('path');
 
-glob('./dist-server/styles.*.bundle.css', { nodir: true }, (err, files) => {    
+glob('./dist/styles.*.bundle.css', { nodir: true }, (err, files) => {    
     const bundleName = files[0].split('/').pop();
     const bundleLink = '<link href="' + bundleName + '" rel="stylesheet">';
     const serverIndex = index.replace('<!--css-bundle-->', bundleLink);
